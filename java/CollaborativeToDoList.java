79cc1a4f05d0822c370ba8d323798c7bb8ce5653
harsha101195 <st3364@columbia.edu>
2025-07-19 10:00:31 -0400

    initial commit

---
+++ java/CollaborativeToDoList.java
@@ -0,0 +1,8 @@
+public class CollaborativeToDoList {
+    public static void main(String[] args) {
+        //Main method to run the Collaborative To-Do List application
+        System.out.println("Welcome to the Collaborative To-Do List Application!");
+        
+    }
+
+}
---
+++ javaScript/collaborative_todo_list.js
@@ -0,0 +1,7 @@
+
+async function main() {
+    //Main function to run the Collaborative To-Do List application
+    console.log("Welcome to the Collaborative To-Do List Application!");
+}
+
+main().catch(console.error);
